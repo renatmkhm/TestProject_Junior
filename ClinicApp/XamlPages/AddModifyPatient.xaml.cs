a9c9cf9817a1fa81d028235142af0cd298619b24
renatmkhm <renat_mkhm@outlook.com>
2023-09-12 10:55:09 +0300

    Update AddModifyPatient.xaml.cs
    fixed bug with application crash when entering invalid date of birth

--- ClinicApp/XamlPages/AddModifyPatient.xaml.cs
+++ ClinicApp/XamlPages/AddModifyPatient.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Media;
 using ClinicApp.EntityModels;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
 
 namespace ClinicApp.XamlPages
 {
@@ -72,6 +73,11 @@ namespace ClinicApp.XamlPages
                 {
                     status = false;
                     messageBuilder.Append("Дата рождения - обязательное поле для ввода.\n");
+                }
+                if (dateOfBirth > DateTime.Now || dateOfBirth < (DateTime)SqlDateTime.MinValue)
+                {
+                    status = false;
+                    messageBuilder.Append("Введите действительную дату рождения.\n");
                 }
                 #endregion
                 #region Checking entered address
